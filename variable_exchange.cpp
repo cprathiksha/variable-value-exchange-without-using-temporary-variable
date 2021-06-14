b78ae0db5ee60248d8ea8346054fdb357ce872f7
Prathiksha Poojary <47886532+cprathiksha@users.noreply.github.com>
2021-06-14 21:23:10 +0530

    Update variable_exchange.cpp

--- variable_exchange.cpp
+++ variable_exchange.cpp
@@ -6,10 +6,10 @@ int main(){
     cin>>variable_one;
     cout<<"\nEnter the value for variable_two :";
     cin>>variable_two;
-    variable_one = variable_one * variable_two; // 1 * 2 = 2
-    variable_two = variable_one / variable_two; //2 / 2 = 1
-    variable_one = variable_one / variable_two; // 2 / 1 = 2
+    variable_one = variable_one * variable_two;
+    variable_two = variable_one / variable_two; 
+    variable_one = variable_one / variable_two; 
     cout<<"A :"<<variable_one;
-    cout<<"\nB :"<<variable_two;
+    cout<<endl<<"B :"<<variable_two;
 
 }
